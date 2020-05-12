10c14bfc240fd62a7de3743a811d60f7ccd7c3c5
Indra Kumar Prajapat <indrakumarprajapat@gmail.com>
2020-05-12 11:18:30 +0530

    Revert "added config file and set all the configration of database and mails"

--- mail.php
+++ mail.php
@@ -1,5 +1,4 @@
 <?php  
-  include 'config.php';
   $name = $_POST["name"];
   $email = $_POST["email"];
   $subject = $_POST["sub"];
@@ -25,9 +24,9 @@
   This message contains information that may be privileged or confidential and is the property of the Tvarit GmbH. It is intended only for the person to whom it is addressed. If you are not the intended recipient, you are not authorized to read, print, retain copy, disseminate, distribute, or use this message or any part thereof. If you receive this message in error, please notify the sender immediately and delete all copies of this message.
 ";
 
-  $headers  = "From: $mail_from\r\n" .
+  $headers  = "From: info@tvarit.com\r\n" .
   "X-Mailer: php\r\n";
-  $headers .= "Bcc: $mail_from\r\n";
+  $headers .= "Bcc: info@tvarit.com\r\n";
 
   mail($to,$subject,$message,$headers);
    
